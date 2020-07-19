2b5c7164268d416a5b7a5ffbbb263f7be78b86df
Zyo86 <zyothebest86@gmail.com>
2020-07-19 20:59:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# console-log-my-food
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/console-log-my-food)
---
+++ index.js
@@ -0,0 +1,12 @@
+#!usr/bin/env Node
+
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Console-log-my-food</h1>`;
+
+
+
+
